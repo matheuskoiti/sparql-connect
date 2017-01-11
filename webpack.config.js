29d858a9637b0eebe4731704469fe82361c65d63
Jean-Baptiste RUDANT <jb@noknot.fr>
2017-01-11 11:29:04 +0100

    Use `webpack` to build
    `jison` requires `fs` module and applications using `sparql-connect` in the browser needed to add an option in their `webpack.config.js` to avoid an error while resolving `fs` module. So we switched to `webpack` here (instead of regular `babel` script), with this `node.fs` option, so other applications do not need to care about it.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+  entry:'./src/index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: ['babel'],
+        query: {
+          'presets': ['react', 'es2015'],
+          'plugins': ['transform-object-rest-spread']
+        },
+      }
+    ]
+  },
+  //`jison` module pattern adds `require('fs')`, which throws an error
+  node: {
+    fs: 'empty'
+  },   
+  output: {
+    path: __dirname + '/lib',
+    filename: './sparql-connect.js'
+  }
+}
